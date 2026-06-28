da0f46bf8ed644335ea6cd80cba6c721c7cdfd34
cellsparkas-spec <cellsparkas@gmail.com>
2026-06-28 11:27:49 +0700

    Update index.ts

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -8,7 +8,12 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL must be set.");
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 export const db = drizzle(pool, { schema });
 
 export * from "./schema/index.js";
